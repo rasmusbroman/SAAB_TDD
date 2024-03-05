45321960ce4b8990c5cc293cf7891c294542aafd
o7Bane <o7bane@outlook.com>
2024-03-05 14:46:30 +0100

    adding time and string

--- SAAB_TDD.Test/SAAB_TDDTest.cs
+++ SAAB_TDD.Test/SAAB_TDDTest.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using NUnit.Framework;
 
 namespace SAAB_TDD.Test
 {
@@ -11,10 +12,47 @@ namespace SAAB_TDD.Test
             SAAB = new();
         }
 
-        [Test]
-        public void CheckTime()
+        [TestCase(23, 59, 59, ExpectedResult = true)]
+        public bool IsValid(int hour, int minute, int second)
         {
+            return SAAB.IsValid(hour, minute, second);
+        }
 
+        // ExpectedResult antas alltid visas med HH:MM:SS, pm
+        [TestCase(23, 51, 59, "am", ExpectedResult = "11:51:59[am]")]
+        [TestCase(9, 52, 59, "am", ExpectedResult = "09:52:59[am]")]
+        [TestCase(23, 53, 59, "pm", ExpectedResult = "11:53:59[pm]")]
+        [TestCase(23, 54, 59, "", ExpectedResult = "23:54:59")]
+        [TestCase(09, 55, 59, "", ExpectedResult = "09:55:59")]
+        [TestCase(9, 56, 59, "", ExpectedResult = "09:56:59")]
+        [TestCase(9, 57, 59, "", ExpectedResult = "09:57:59")]
+        [TestCase(12, 18, 59, "pm", ExpectedResult = "00:18:59[pm]")]
+        [TestCase(0, 59, 59, "pm", ExpectedResult = "00:59:59[pm]")]
+        public string TimeToString(int hour, int minute, int second, string amPm)
+        {
+            return SAAB.TimeToString(hour, minute, second, amPm);
         }
     }
 }
+
+
+
+
+
+
+
+        //[Test]
+        //public void IsTthisValid()
+        //{
+        //    SAAB_TDD time = new(23, 10, 15);
+        //    //time.ToString();
+        //    Assert.IsTrue(time.IsThisValid());
+        //}
+
+        //[TestCase(23, 10, 15, ExpectedResult = true)]
+        //public bool IsTimeValid(int hours, int minutes, int seconds)
+        //{
+        //    SAAB_TDD time = new SAAB_TDD(hours, minutes, seconds);
+        //    //time.ToString();
+        //    return time.IsThisValid();
+        //}
--- SAAB_TDD/SAAB_TDD.cs
+++ SAAB_TDD/SAAB_TDD.cs
@@ -1,11 +1,80 @@
-﻿namespace SAAB_TDD
+﻿
+namespace SAAB_TDD
 {
     public class SAAB_TDD
     {
+        public int minute;
+        public int second;
+        public int hour;
 
-        public void WhatsTheTime()
+        public SAAB_TDD() { }
+
+        public SAAB_TDD(int hour, int minute, int second)
         {
-            
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public string TimeToString(int hour, int minute, int second, string amPm)
+        {
+            if (IsValid(hour, minute, second) == true)
+            {
+                string hourBelowTen = "";
+                if (amPm.Contains("pm") || amPm.Contains("am"))
+                {
+                    int displayHour = hour > 12 ? hour - 12 : hour == 12 ? 0 : hour;
+                    if (displayHour < 10)
+                    {
+                        //string[] delimiters = { "[", "]" };
+                        ////= stringHour.Split('[', ']').Trim();
+                        ////nums = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                        //hourBelowTen = displayHour(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                        //hourBelowTen = "0" + displayHour.ToString();
+                        //string showTime = $"{hourBelowTen}:{minute}:{second}: {amPm}";
+                        //return showTime;
+                        hourBelowTen = "0" + displayHour.ToString();
+                        return $"{hourBelowTen}:{minute}:{second}[{amPm}]";
+                    }
+                    return $"{displayHour}:{minute}:{second}[{amPm}]";
+                }
+
+                else if (hour < 10)
+                {
+                    hourBelowTen = "0" + hour.ToString();
+                    return $"{hourBelowTen}:{minute}:{second}";
+                }
+                return $"{hour}:{minute}:{second}";
+            }
+            else
+            {
+                throw new ArgumentException("Time is not written correct");
+            }
+        }
+
+        public bool IsValid(int hour, int minute, int second)
+        {
+            if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60 && second >= 0 && second < 60)
+            {
+                return true;
+            }
+            throw new ArgumentException("Time is not correct");
         }
     }
 }
+
+
+
+
+
+
+
+
+//public bool IsThisValid()
+//{
+//    if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60 && second >= 0 && second < 60)
+//    {
+//        return true;
+//    }
+//    throw new ArgumentException("Time is not correct");
+//}
